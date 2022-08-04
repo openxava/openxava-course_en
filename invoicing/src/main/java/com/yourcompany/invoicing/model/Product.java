b70f5170bb55b07fefcb672b8f591a1cf0b9db00
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-04 11:51:10 -0300

    Lesson 2

--- invoicing/src/main/java/com/yourcompany/invoicing/model/Product.java
+++ invoicing/src/main/java/com/yourcompany/invoicing/model/Product.java
@@ -1,5 +1,7 @@
 package com.yourcompany.invoicing.model;
  
+import java.math.*;
+
 import javax.persistence.*;
 
 import org.openxava.annotations.*;
@@ -20,5 +22,15 @@ public class Product {
             optional=true) // The reference can have no value
     @DescriptionsList // Thus the reference is displayed using a combo
     Category category; // A regular Java reference
+    
+    @Money // The price property is used to store money
+    BigDecimal price;  // BigDecimal is typically used for money
+
+    @Files // A complete image gallery is available
+    @Column(length=32) // The 32 length string is for storing the key of the gallery
+    String photos;
+     
+    @TextArea// This is for a big text, a text area or equivalent will be used
+    String remarks;
  
 }
