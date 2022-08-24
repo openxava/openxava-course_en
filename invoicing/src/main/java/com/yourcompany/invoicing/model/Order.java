f22c18095e7f3f74d058ae0ce2a56c220de4c021
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-24 11:16:35 -0300

    OX 7 Lesson 16

--- invoicing/src/main/java/com/yourcompany/invoicing/model/Order.java
+++ invoicing/src/main/java/com/yourcompany/invoicing/model/Order.java
@@ -1,5 +1,7 @@
 package com.yourcompany.invoicing.model;
 
+import java.time.*;
+
 import javax.persistence.*;
 
 import org.openxava.annotations.*;
@@ -7,8 +9,10 @@ import org.openxava.annotations.*;
 import lombok.*;
 
 @Entity	 @Getter @Setter
-@View( extendsView="super.DEFAULT", // The default view
-members="invoice { invoice } "
+@View(extendsView="super.DEFAULT", 
+members=
+    "estimatedDeliveryDays," + // ADD THIS LINE
+    "invoice { invoice }"
 )
 @View( name="NoCustomerNoInvoice", // A view named NoCustomerNoInvoice
 members=                       // that does not include customer and invoice.
@@ -22,4 +26,21 @@ public class Order extends CommercialDocument{
     @ReferenceView("NoCustomerNoOrders") // This view is used to display invoice
     private Invoice invoice;
 	
+    @Depends("date")
+    public int getEstimatedDeliveryDays() {
+        if (getDate().getDayOfYear() < 15) {
+            return 20 - getDate().getDayOfYear(); 
+        }
+        if (getDate().getDayOfWeek() == DayOfWeek.SUNDAY) return 2;
+        if (getDate().getDayOfWeek() == DayOfWeek.SATURDAY) return 3;
+        return 1;
+    }
+    
+    @Column(columnDefinition="INTEGER DEFAULT 1")
+    int deliveryDays;
+    
+    @PrePersist @PreUpdate 
+    private void recalculateDeliveryDays() {
+        setDeliveryDays(getEstimatedDeliveryDays());
+    }
 }
