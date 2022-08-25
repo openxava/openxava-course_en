a94a4d05adb1db4ce4b1d3cac15ebfa0d9ad4068
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-25 13:05:33 -0300

    OX 7 Lesson 18

--- invoicing/src/main/java/com/yourcompany/invoicing/model/Order.java
+++ invoicing/src/main/java/com/yourcompany/invoicing/model/Order.java
@@ -20,8 +20,6 @@ members=                       // that does not include customer and invoice.
     "details;" +
     "remarks"
 )
-//Validation alternative with validator,  if use this, you have to change in i18n for
-//order_must_be_delivered=Order {0}/{1} must be delivered in order to be added to an Invoice
 @EntityValidator(
 	    value=com.yourcompany.invoicing.validators.DeliveredToBeInInvoiceValidator.class, // The class with the validation logic
 	    properties= {
@@ -50,8 +48,6 @@ public class Order extends CommercialDocument{
     @Column(columnDefinition="INTEGER DEFAULT 1")
     int deliveryDays;
     
-    //if use Validation alternative with JPA callback method
-    //have to comment @PrePersist @PreUpdate 
     @PrePersist @PreUpdate 
     private void recalculateDeliveryDays() {
         setDeliveryDays(getEstimatedDeliveryDays());
@@ -59,44 +55,5 @@ public class Order extends CommercialDocument{
     
     @Column(columnDefinition="BOOLEAN DEFAULT FALSE")
     boolean delivered;
-    
-//   //Validation alternative with JPA callback method
-//    @PrePersist @PreUpdate // Just before creating or updating
-//    private void validate()  throws Exception {
-//        if (invoice != null && !isDelivered()) { // The validation logic
-//            // The validation exception from Bean Validation
-//            throw new javax.validation.ValidationException(
-//                XavaResources.getString(
-//                    "order_must_be_delivered",
-//                    getYear(),
-//                    getNumber()
-//                )
-//            );
-//        }
-//    }
-    
-//   //Validation alternative with setter
-//    public void setInvoice(Invoice invoice) {
-//        if (invoice != null && !isDelivered()) { // The validation logic
-//            // The validation exception from Bean Validation
-//            throw new javax.validation.ValidationException(
-//                XavaResources.getString(
-//                    "order_must_be_delivered",
-//                    getYear(),
-//                    getNumber()
-//                )
-//            );
-//        }
-//        this.invoice = invoice; // The regular setter assignment
-//    }
-    
-//   //Validation alternative with Bean Validation, if use this, you have to change in i18n for
-//   //order_must_be_delivered=Order {year}/{number} must be delivered in order to be added to an Invoice
-//    @AssertTrue( // Before saving it asserts if this method returns true, if not it throws an exception
-//    	    message="order_must_be_delivered" // Error message in case false
-//    	)
-//    	private boolean isDeliveredToBeInInvoice() {
-//    	    return invoice == null || isDelivered(); // The validation logic
-//    	}
     
 }
