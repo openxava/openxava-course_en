c05d41eabd931e32ec4ad29a068897476d388cd0
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-08-26 10:44:22 -0300

    OX 7 Lesson 20

--- invoicing/src/main/java/com/yourcompany/invoicing/model/Order.java
+++ invoicing/src/main/java/com/yourcompany/invoicing/model/Order.java
@@ -3,8 +3,10 @@ package com.yourcompany.invoicing.model;
 import java.time.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 import org.openxava.annotations.*;
+import org.openxava.util.*;
 
 import lombok.*;
 
@@ -20,17 +22,6 @@ members=                       // that does not include customer and invoice.
     "details;" +
     "remarks"
 )
-//Validation alternative with validator,  if use this, you have to change in i18n for
-//order_must_be_delivered=Order {0}/{1} must be delivered in order to be added to an Invoice
-@EntityValidator(
-	    value=com.yourcompany.invoicing.validators.DeliveredToBeInInvoiceValidator.class, // The class with the validation logic
-	    properties= {
-	        @PropertyValue(name="year"), // The content of these properties
-	        @PropertyValue(name="number"), // is moved from the 'Order' entity
-	        @PropertyValue(name="invoice"), // to the validator before
-	        @PropertyValue(name="delivered") // executing the validation
-	    }
-	)
 public class Order extends CommercialDocument{
 
     @ManyToOne
@@ -59,44 +50,24 @@ public class Order extends CommercialDocument{
     
     @Column(columnDefinition="BOOLEAN DEFAULT FALSE")
     boolean delivered;
+
+    //Validation alternative with Bean Validation, if use this, you have to change in i18n for
+    //order_must_be_delivered=Order {year}/{number} must be delivered in order to be added to an Invoice
+    @AssertTrue( // Before saving it asserts if this method returns true, if not it throws an exception
+    	    message="order_must_be_delivered" // Error message in case false
+    	)
+    	private boolean isDeliveredToBeInInvoice() {
+    	    return invoice == null || isDelivered(); // The validation logic
+    	}
     
-//   //Validation alternative with JPA callback method
-//    @PrePersist @PreUpdate // Just before creating or updating
-//    private void validate()  throws Exception {
-//        if (invoice != null && !isDelivered()) { // The validation logic
-//            // The validation exception from Bean Validation
-//            throw new javax.validation.ValidationException(
-//                XavaResources.getString(
-//                    "order_must_be_delivered",
-//                    getYear(),
-//                    getNumber()
-//                )
-//            );
-//        }
-//    }
-    
-//   //Validation alternative with setter
-//    public void setInvoice(Invoice invoice) {
-//        if (invoice != null && !isDelivered()) { // The validation logic
-//            // The validation exception from Bean Validation
-//            throw new javax.validation.ValidationException(
-//                XavaResources.getString(
-//                    "order_must_be_delivered",
-//                    getYear(),
-//                    getNumber()
-//                )
-//            );
-//        }
-//        this.invoice = invoice; // The regular setter assignment
-//    }
-    
-//   //Validation alternative with Bean Validation, if use this, you have to change in i18n for
-//   //order_must_be_delivered=Order {year}/{number} must be delivered in order to be added to an Invoice
-//    @AssertTrue( // Before saving it asserts if this method returns true, if not it throws an exception
-//    	    message="order_must_be_delivered" // Error message in case false
-//    	)
-//    	private boolean isDeliveredToBeInInvoice() {
-//    	    return invoice == null || isDelivered(); // The validation logic
-//    	}
+    //Validation alternative with JPA callback method
+    @PreRemove // Just before removing the entity
+    private void validateOnRemove() {
+        if (invoice != null) { // The validation logic
+            throw new javax.validation.ValidationException( // Throws a runtime exception
+                XavaResources.getString( // To get the text message
+                    "cannot_delete_order_with_invoice"));
+        }
+    }
     
 }
